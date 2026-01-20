2f663c8b27c953fdfd0ed09c0bc497b92c8bc3c5
gitika0612 <67464641+gitika0612@users.noreply.github.com>
2026-01-20 19:50:51 +0530

    added web analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route index element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+      <Analytics />
     </>
   );
 }
